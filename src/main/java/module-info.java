a990a58a6f47375e3ba3f9d8f0329780b1cee0dd
tduernbe <tduernbe@htl-steyr.ac.at>
2024-12-03 14:12:08 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example._2425_fsst_5ahel_tduernbe_kantenst_viergewinnt {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example._2425_fsst_5ahel_tduernbe_kantenst_viergewinnt to javafx.fxml;
+    exports org.example._2425_fsst_5ahel_tduernbe_kantenst_viergewinnt;
+}
